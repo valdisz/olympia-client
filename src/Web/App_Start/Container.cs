0677965a8f953edd64b0efe9067f99f2d66bce9c
Valdis Zobēla <valdis.zobela@gmail.com>
2013-08-26 23:22:06 +0300

    Added Autofac and embedded RavenDB

---
+++ src/Web/App_Start/Container.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.App_Start
+{
+    using Autofac;
+
+    using Raven.Client;
+    using Raven.Client.Embedded;
+
+    public class Container : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+
+            builder.Register(ctx =>
+            {
+                var store = new EmbeddableDocumentStore
+                {
+                    DataDirectory = "~/App_Data"
+                };
+                store.Initialize();
+            
+                return store;
+            }).As<IDocumentStore>().SingleInstance();
+        }
+    }
+}
--- src/Web/Global.asax.cs
+++ src/Web/Global.asax.cs
@@ -1,19 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
-using System.Web.Routing;
-
-namespace Web
+﻿namespace Web
 {
+    using System.Web;
+    using System.Web.Http;
+    using System.Reflection;
+
+    using Autofac;
+    using Autofac.Integration.WebApi;
+
     // Note: For instructions on enabling IIS7 classic mode, 
     // visit http://go.microsoft.com/fwlink/?LinkId=301868
-    public class WebApiApplication : System.Web.HttpApplication
+    public class WebApiApplication : HttpApplication
     {
+        private static IContainer container;
+
         protected void Application_Start()
         {
-            WebApiConfig.Register(GlobalConfiguration.Configuration);           
+            var assembly = Assembly.GetExecutingAssembly();
+
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+
+
+            var builder = new ContainerBuilder();
+            builder.RegisterApiControllers(assembly);
+            builder.RegisterWebApiFilterProvider(GlobalConfiguration.Configuration);
+            builder.RegisterAssemblyModules(assembly);
+            
+            container = builder.Build();
+            GlobalConfiguration.Configuration.DependencyResolver =
+                new AutofacWebApiDependencyResolver(container);
         }
     }
 }
