cd66b84641b924dc74a50aac75030316da4f3451
Valdis Zobēla <valdis.zobela@gmail.com>
2013-08-27 22:51:18 +0300

    report uploading

--- src/Web/app/olympia.ts
+++ src/Web/app/olympia.ts
@@ -5,7 +5,55 @@ declare var angular;
 
 module Olympia {
     angular.module('Olympia', [])
-    .controller('main', function() { })
+        .controller('main', function ($scope, $http) {
+            $scope.upload = function () {
+                $('#fileUploadForm input').click();
+            };
+
+            $scope.uploadReport = function () {
+                $http({
+                    method: 'POST',
+                    url: "api/upload",
+                    headers: { 'Content-Type': false },
+                    transformRequest: function (data) {
+                        var formData = new FormData();
+                        formData.append("file", data.file);
+                        return formData;
+                    },
+                    data: { file: $scope.reportFile }
+                }).
+                    success(function (data, status, headers, config) {
+                        alert("success!");
+                    }).
+                    error(function (data, status, headers, config) {
+                        alert("failed!");
+                    });
+            };
+        })
+
+        .directive('fileInput', function ($parse) {
+            return {
+                restrict: 'EA',
+                template: '<input type="file" />',
+                replace: true,
+                link: function (scope, element, attrs) {
+
+                    var modelGet = $parse(attrs.fileInput);
+                    var modelSet = modelGet.assign;
+                    var onChange = $parse(attrs.onChange);
+
+                    var updateModel = function () {
+                        scope.$apply(function () {
+                            modelSet(scope, element[0].files[0]);
+                            onChange(scope);
+                        });
+                    };
+
+                    element.bind('change', updateModel);
+                }
+            };
+        })
+
         .directive('craftyCanvas', function () {
         return {
             restrict: 'E',
@@ -18,7 +66,6 @@ module Olympia {
 
                 window.onresize = function () {
                     Crafty.viewport.init(element.parent().width(), $(window).height() - 50);
-                    console.log('resize');
                 };
             }
         };
